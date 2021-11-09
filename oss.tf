a16fc941900f6a9605e28f3cf620d4cf505f63c5
寒玄 <hanxuan.mh@alibaba-inc.com>
2021-11-09 14:51:05 +0800

    init

--- oss.tf
+++ oss.tf
@@ -1,9 +1,3 @@
-#provider "alicloud" {
-#  access_key = "LTAI5tSXcf4M4T8us8j9HWC9"
-#  secret_key = "qHdKUQaHkPa6l4yVlW9HglwOXPhjID"
-#  region = "cn-hangzhou"
-#}
-
 resource "alicloud_oss_bucket" "hanxuan-test-2" {
   bucket = "hanxuan-test-2"
 }
